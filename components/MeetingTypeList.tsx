48cb76c8d91f5bdc9e0c8304291b3340064c52bf
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-04-08 21:15:39 +0530

    initial Commit

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,6 +6,7 @@ import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { useToast } from "@/components/ui/use-toast"
 import { Textarea } from './ui/textarea'
+import { Input } from "@/components/ui/input"
 
 
 import HomeCard from './HomeCard'
@@ -150,6 +151,21 @@ const MeetingTypeList = () => {
                 buttonText='Start Meeting'
                 handleClick={createMeeting}
             />
+
+            <MeetingModal 
+                isOpen={meetingState === 'isJoiningMeeting'}
+                onClose={() => setMeetingState(undefined)}
+                title="Type the link here"
+                className='text-center'
+                buttonText='Join Meeting'
+                handleClick={() => router.push(values.link)}
+            >
+                <Input 
+                    placeholder='Meeting Link'
+                    className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+                    onChange={(e) => setValues({ ...values, link: e.target.value })}
+                />
+            </MeetingModal>
         </section>
     )
 }
